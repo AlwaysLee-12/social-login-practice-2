e95d95cde3f4fd32f0cf5ca359cbf844840519a9
alwayslee <ddoddo1997@naver.com>
2021-09-28 14:27:36 +0900

    Feat: Auth에 Swagger 코드 적용

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,5 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Put,
-  Req,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get, Put, Req, UseGuards } from '@nestjs/common';
+import { ApiDocs } from './auth.docs';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { JwtRefreshGuard } from './jwt-refresh-auth.guard';
@@ -17,6 +9,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get('kakao')
+  @ApiDocs.kakaoLogin('카카오 로그인')
   async kakaoLogin(@Req() req: any) {
     const kakaoUserData = await this.authService.isValidKakaoToken(
       req.headers.authorization,
@@ -47,18 +40,21 @@ export class AuthController {
   // }
 
   @Put('logout')
+  @ApiDocs.logout('로그아웃')
   async logout(@Req() req: any): Promise<void> {
     this.authService.logout(req.user);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
+  @ApiDocs.getProfile('프로필 조회')
   getProfile(@Req() req: any) {
     return req.user;
   }
 
   @UseGuards(JwtRefreshGuard)
   @Get('refresh')
+  @ApiDocs.refreshToken('토큰 리프레시')
   refreshToken(@Req() req: any) {
     const newAccessToken = this.authService.refresh(req.user);
     return { access_token: newAccessToken };
