32ffdc899077e30d733f605716d3480f88d181c6
alwayslee <ddoddo1997@naver.com>
2021-09-28 13:45:07 +0900

    Chore: Swagger Setting(스웨거 연습)

---
+++ src/swagger/constants.ts
@@ -0,0 +1,5 @@
+export const SWAGGER_API_ROOT = 'api';
+export const SWAGGER_API_TITLE = 'always OAuth API';
+export const SWAGGER_API_DESCRIPTION = 'The OAuth API description';
+export const SWAGGER_API_CURRENT_VERSION = '1.0';
+export const SWAGGER_API_TAG = 'always';
