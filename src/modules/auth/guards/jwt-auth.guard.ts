869114bf75a1b82b7d5f9ca2a39f83ea8fab1924
alwayslee <ddoddo1997@naver.com>
2021-10-14 14:27:39 +0900

    Feat: 토큰 만료여부 따로 에러처리

--- src/modules/auth/guards/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -7,12 +7,13 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  //주로 현재 context의 request에 접근하기 위한 용도로 boolean을 리턴
   canActivate(context: ExecutionContext) {
     return super.canActivate(context);
   }
-
+  //기본 오류 처리에서 보다 더 확장하고싶거나 인증 논리를 추가하려는 경우
   handleRequest(err, user, info) {
-    console.log(user + '' + info);
+    //console.log(user + '' + info);
     if (err || !user) {
       throw err || new UnauthorizedException('Invalid Token Error');
     }
--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 
@@ -7,12 +7,17 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      IgnoreExpiration: false,
+      IgnoreExpiration: true,
       secretOrKey: process.env.JWT_ACCESS_TOKEN_SECRET,
     });
   }
 
   async validate(payload: any) {
+    console.log(payload);
+    const currentNumericDate: number = new Date().getTime();
+    if (payload.exp < currentNumericDate) {
+      throw new HttpException('Token Expired', HttpStatus.UNAUTHORIZED);
+    }
     return { userId: payload.user_id, username: payload.sub };
   }
 }
